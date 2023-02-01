a0fc7e78dafb250a0e513d58cc9b25f4b78f7f5c
Scott Henderson <scott.mc.henderson@gmail.com>
2023-01-31 19:25:26 -0800

    update .env file

--- src/Components/SearchRecipe.jsx
+++ src/Components/SearchRecipe.jsx
@@ -12,14 +12,8 @@ function SearchRecipe() {
     //initializing state to store user input value
 
     const getRecipe = (event) => {
-        // console.log('API called');
         event.preventDefault()
-        const APP_ID = "0f25f43a";
-        const APP_KEY = "4638dc3291ceb38bd729a2d8d0bb4fbd";
-        console.log(process.env.REACT_APP_ID_VAR)
-        console.log('test');
-
-        axios.get(`https://api.edamam.com/api/recipes/v2?type=public&app_id=${APP_ID}&app_key=${APP_KEY}&q=${inputValue}&calories=500-1000&imageSize=SMALL&time=10-60`,
+        axios.get(`https://api.edamam.com/api/recipes/v2?type=public&app_id=${process.env.REACT_APP_ID_VAR}&app_key=${process.env.REACT_APP__KEY}&q=${inputValue}&calories=500-1000&imageSize=SMALL&time=10-60`,
             {
                 headers: {
                     'Access-Control-Allow-Origin': '*',
