ca579d7803605ce8d27e40812d6ddc1385885a3b
Scott Henderson <scott.mc.henderson@gmail.com>
2025-09-19 04:26:19 -0700

    trying to update pnpm modules

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App';
 import {Provider} from 'react-redux'
-import { createStore} from 'redux'
+import { legacy_createStore as createStore} from 'redux'
 import rootReducer from './redux/reducers/rootReducer';
 
 
@@ -13,12 +13,14 @@ const store = createStore(
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 )
 
-ReactDOM.render(
+const container = document.getElementById('root');
+const root = createRoot(container);
+
+root.render(
   <React.StrictMode>
     <Provider store = {store}>
       <App />
     </Provider>
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
 
